44998e4f77e3c288645c427a1d506e57b7789929
Александр Забейворота <aleksandrzabejvorota@MacBook-Air-Aleksandr.local>
2025-07-13 08:16:29 +0700

    hw2new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,14 @@
 package org.example;
 
-import org.example.pojo.Person;
+
 
 import static org.example.InputPathFile.inputPathFile;
 import static org.example.ReadFile.*;
 
 public class Main {
     public static void main(String[] args) {
-        //String path = "src/main/resources/";
-        String nameInputFile = "file.txt";
-        String nameOutputFile = "output.txt";
         String inputFile = "input.txt";
 
-
-//        StringBuffer changeChar = changeCharacter(readFile(path + nameInputFile));
-//        writeFile(changeChar, path + nameOutputFile);
-//        countCharacter(readFile(path + nameOutputFile));
         while (true){
             String path = inputPathFile();
             StringBuffer newString = readFile(path + inputFile);

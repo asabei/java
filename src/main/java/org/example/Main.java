0439f53313c9a9595af0b04523abd4d3ef87af46
Александр Забейворота <aleksandrzabejvorota@MacBook-Air-Aleksandr.local>
2025-07-18 08:53:53 +0700

    hw4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,47 +6,40 @@ import org.example.Gift.Box;
 import org.example.Gift.Cake;
 import org.example.Gift.Chocolate;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static org.example.InputPathFile.inputPathFile;
 import static org.example.ReadFile.*;
 
 public class Main {
     public static void main(String[] args) {
+        String path = "src/main/resources/text";
+        StringBuffer textFile = readFile(path);
+        String str = textFile.toString();
+        String[] arr = str.split("[\\s\\p{Punct}]+");
+        List<String> strWithoutDidits = Arrays.stream(arr).filter(chars -> !chars.matches("\\d+")).toList();
+        List<String> lowerCaseStr = strWithoutDidits.stream()
+                .map(chars -> chars.toLowerCase())
+                .sorted()
+                .toList();
+        System.out.println("В алфавитном порядке " + lowerCaseStr);
+        Map<String, Long> wordFrequency = lowerCaseStr.stream()
+                .collect(Collectors.groupingBy(
+                        word -> word,
+                        Collectors.counting()
+                ));
+        System.out.println("Статистика " + wordFrequency);
+        List result = wordFrequency.entrySet().stream()
+                .filter(value -> value.getValue() == Collections.max(wordFrequency.values()))
+                .toList();
+        System.out.println("Слово " + result);
 
 
-        Box box = new Box();
-        box.addCandy(new Chocolate("Snickers", 40, 150, "double"));
-        box.addCandy(new Chocolate("Milka", 45, 120, "milk"));
-        box.addCandy(new Cake("Brownie", 90, 1150, "brown"));
 
-        box.getAllInfo();
-        System.out.println("Общая стоимость: " + box.getPriceBox());
-        System.out.println("Общий вес: " + box.getWeightBox());
-
-        InputPathFile inputPathFile = new InputPathFile();
-        int optimizationParam = 0;
-
-        System.out.println("Введите число 1- выбор оптимизиции по весу, 2 - выбор оптимизации по цене ");
-        while (true) {
-            int optimization = inputPathFile.inputChoiceOptimization();
-            if (optimization == 1 || optimization == 2) {
-                System.out.println("Спасибо! Вы ввели: " + optimization);
-                optimizationParam = optimization;
-                break;
-            } else {
-                System.out.println("Ошибка: введите только 1 или 2");
-            }
-        }
-        System.out.println("Введите вес для оптимизации коробки");
-        int optimizationWeight = inputPathFile.inputChoiceOptimization();
-        if (optimizationParam == 1) {
-            box.deleteMinWeight(optimizationWeight);
-        } else {
-            box.deleteMinPrice(optimizationWeight);
-        }
-        System.out.println("\nПосле оптимизации:");
-        box.getAllInfo();
-        System.out.println("Общая стоимость: " + box.getPriceBox());
-        System.out.println("Общий вес: " + box.getWeightBox());
 
     }
 }
